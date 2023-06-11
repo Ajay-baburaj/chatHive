a0711216019b3bb50f31af9b8497cccd0eb557bf
ajay baburaj <ajaybaburajp@gmail.com>
2023-06-12 00:23:41 +0530

    server setup incomplete

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,2 +1,17 @@
 import http from 'http'
 import express,{Application,NextFunction} from 'express'
+import expressConfig from './framework/webserver/express'
+import config  from 'config'
+
+const app:Application = express()
+expressConfig(app)
+const server = http.createServer(app)
+
+const port = config.get<number>('port')
+const host = config.get<string>('host')
+
+
+
+server.listen(port,host,()=>{
+    console.log('server is running')
+})
