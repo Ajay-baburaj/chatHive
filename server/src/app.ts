bb605e64ba4e5547c8970a7231c148f492920c8f
ajay baburaj <ajaybaburajp@gmail.com>
2023-06-12 16:19:06 +0530

    graphql setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,30 @@
 import http from 'http'
 import express,{Application,NextFunction} from 'express'
+import dotenv from 'dotenv'
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
 import expressConfig from './framework/webserver/express'
 
+dotenv.config();
 const app:Application = express()
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+    hello: () => 'Hello, GraphQL!'
+  };
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true 
+  }));
 expressConfig(app)
 const server = http.createServer(app)
+  
 
-server.listen(3000, () => {
-    console.log('Port is running');
+server.listen(process.env.PORT, () => {
+    console.log(`Port is running at ${process.env.PORT}`);
   });
