9f2c898e4f34fda811ddfddcacbba088733221de
ajay baburaj <ajaybaburajp@gmail.com>
2023-06-12 15:25:57 +0530

    port ser

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,11 @@
 import http from 'http'
 import express,{Application,NextFunction} from 'express'
 import expressConfig from './framework/webserver/express'
-import config  from 'config'
 
 const app:Application = express()
 expressConfig(app)
 const server = http.createServer(app)
 
-const port = config.get<number>('port')
-const host = config.get<string>('host')
-
-
-
-server.listen(port,host,()=>{
-    console.log('server is running')
-})
+server.listen(3000, () => {
+    console.log('Port is running');
+  });
